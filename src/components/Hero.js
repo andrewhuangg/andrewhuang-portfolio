07ff35d0d9237b4cf43b11fcbdb202a406ee9e0d
Andrew <drewhuangg@gmail.com>
2021-09-16 18:51:40 -0700

    add gsap react-gsap and test components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,30 +1,58 @@
 import React from 'react';
 import HeadShot from '../assets/images/headshot.jpg';
 import Parallax from 'react-rellax';
+import { Tween } from 'react-gsap';
 
 const Hero = () => {
   return (
     <>
       <div className='hero'>
         <Parallax centered speed={-9} className='hero__pllx hero__pllx-img1' />
-        <Parallax centered speed={-7} className='hero__pllx hero__pllx-img2' />
-        <Parallax centered speed={-6} className='hero__pllx hero__pllx-img3' />
+        <Parallax centered speed={-8} className='hero__pllx hero__pllx-img2' />
+        <Parallax centered speed={-7} className='hero__pllx hero__pllx-img3' />
         <Parallax centered speed={-3} className='hero__pllx hero__pllx-img4' />
 
         <section className='hero__about'>
           <div className='hero__content'>
-            <Parallax centered speed={-6} className='hero__img-container'>
-              <img src={HeadShot} className='hero__img' draggable='false' />
+            <Parallax centered speed={-8} className='hero__img-container'>
+              <Tween
+                from={('.hero__img-container', { opacity: 0, delay: 0.8, y: 35 })}
+                duration={1.5}
+                ease='expo.out'
+              >
+                <img src={HeadShot} className='hero__img' draggable='false' />
+              </Tween>
             </Parallax>
+
             <Parallax centered speed={-6} className='hero__title'>
-              <h3>Andrew Huang</h3>
+              <Tween
+                from={('.hero__title', { opacity: 0, delay: 0.7, y: 35 })}
+                duration={1.5}
+                ease='expo.out'
+              >
+                <h3>Andrew Huang</h3>
+              </Tween>
             </Parallax>
+
             <Parallax centered speed={-5} className='hero__subtitle'>
-              <p>Software Developer</p>
+              <Tween
+                from={('.hero__subtitle', { opacity: 0, delay: 0.6, y: 35 })}
+                duration={1.5}
+                ease='expo.out'
+              >
+                <p>Software Developer</p>
+              </Tween>
             </Parallax>
+
             {/* Attach Link to Resume */}
             <Parallax centered speed={-5} className='hero__cta'>
-              <a href='#'>Download My Resume!</a>
+              <Tween
+                from={('.hero__cta', { opacity: 0, delay: 0.5, y: 35 })}
+                duration={1.5}
+                ease='expo.out'
+              >
+                <a href='#'>Download My Resume!</a>
+              </Tween>
             </Parallax>
           </div>
         </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Tween } from 'react-gsap';
 
 const Navbar = ({ isToggled, setIsToggled }) => {
   const handleToggle = (e) => {
@@ -8,14 +9,26 @@ const Navbar = ({ isToggled, setIsToggled }) => {
 
   return (
     <div className={`navbar ${isToggled && 'navbar--active'}`}>
-      <a href='#' className='navbar__logo'>
-        Andrew Huang
-      </a>
+      <Tween
+        from={('.navbar__logo', { opacity: 0, delay: 0.8, y: 5 })}
+        duration={1.5}
+        ease='expo.out'
+      >
+        <a href='#' className='navbar__logo'>
+          Andrew Huang
+        </a>
+      </Tween>
       <div
         className={`navbar__toggle ${isToggled && 'navbar__toggle--active'}`}
         onClick={(e) => handleToggle(e)}
       >
-        <i className='fas fa-bars' />
+        <Tween
+          from={('.navbar__toggle', { opacity: 0, delay: 0.8, y: 5 })}
+          duration={1.5}
+          ease='expo.out'
+        >
+          <i className='fas fa-bars' />
+        </Tween>
       </div>
     </div>
   );
