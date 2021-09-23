0f0de335d4a1676a7b1163f556059e04af9c5766
Andrew <drewhuangg@gmail.com>
2021-09-23 15:49:57 -0700

    hero mobile style, revert back to react-rellax

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,50 +1,20 @@
 import React from 'react';
 import HeadShot from '../assets/images/headshot.jpg';
 import Parallax from 'react-rellax';
-import RellaxWrapper from 'react-rellax-wrapper';
 import { Tween } from 'react-gsap';
 
 const Hero = () => {
   return (
     <>
       <div className='hero'>
-        <RellaxWrapper
-          speed={-9}
-          mobile={-8}
-          breakpoints={[639, 1023, 1399]}
-          vertical
-          className='hero__pllx hero__pllx-img1'
-        />
-        <RellaxWrapper
-          speed={-8}
-          mobile={-7}
-          breakpoints={[639, 1023, 1399]}
-          vertical
-          className='hero__pllx hero__pllx-img2'
-        />
-        <RellaxWrapper
-          speed={-7}
-          mobile={-6}
-          breakpoints={[639, 1023, 1399]}
-          vertical
-          className='hero__pllx hero__pllx-img3'
-        />
-        <RellaxWrapper
-          speed={-3}
-          mobile={-2}
-          breakpoints={[639, 1023, 1399]}
-          vertical
-          className='hero__pllx hero__pllx-img4'
-        />
+        <Parallax centered speed={-9} className='hero__pllx hero__pllx-img1' />
+        <Parallax centered speed={-8} className='hero__pllx hero__pllx-img2' />
+        <Parallax centered speed={-7} className='hero__pllx hero__pllx-img3' />
+        <Parallax centered speed={-3} className='hero__pllx hero__pllx-img4' />
+
         <section className='hero__about'>
           <div className='hero__content'>
-            <RellaxWrapper
-              speed={-8}
-              mobile={-7}
-              breakpoints={[639, 1023, 1399]}
-              vertical
-              className='hero__img-container'
-            >
+            <Parallax centered speed={-8} className='hero__img-container'>
               <Tween
                 from={('.hero__img-container', { opacity: 0, delay: 0.8, y: 35 })}
                 duration={1.5}
@@ -52,15 +22,9 @@ const Hero = () => {
               >
                 <img src={HeadShot} className='hero__img' draggable='false' />
               </Tween>
-            </RellaxWrapper>
+            </Parallax>
 
-            <RellaxWrapper
-              speed={-6}
-              mobile={-5}
-              breakpoints={[639, 1023, 1399]}
-              vertical
-              className='hero__title'
-            >
+            <Parallax centered speed={-6} className='hero__title'>
               <Tween
                 from={('.hero__title', { opacity: 0, delay: 0.7, y: 35 })}
                 duration={1.5}
@@ -68,15 +32,9 @@ const Hero = () => {
               >
                 <h3>Andrew Huang</h3>
               </Tween>
-            </RellaxWrapper>
+            </Parallax>
 
-            <RellaxWrapper
-              speed={-4}
-              mobile={-3}
-              breakpoints={[639, 1023, 1399]}
-              vertical
-              className='hero__subtitle'
-            >
+            <Parallax centered speed={-7} className='hero__subtitle'>
               <Tween
                 from={('.hero__subtitle', { opacity: 0, delay: 0.6, y: 35 })}
                 duration={1.5}
@@ -84,16 +42,11 @@ const Hero = () => {
               >
                 <p>Software Developer</p>
               </Tween>
-            </RellaxWrapper>
+            </Parallax>
 
             {/* Attach Link to Resume */}
-            <RellaxWrapper
-              speed={-3}
-              mobile={-2}
-              breakpoints={[639, 1023, 1399]}
-              vertical
-              className='hero__cta'
-            >
+
+            <Parallax centered speed={-3} className='hero__cta'>
               <Tween
                 from={('.hero__cta', { opacity: 0, delay: 0.5, y: 35 })}
                 duration={1.5}
@@ -101,7 +54,7 @@ const Hero = () => {
               >
                 <a href='#'>Download My Resume!</a>
               </Tween>
-            </RellaxWrapper>
+            </Parallax>
           </div>
         </section>
       </div>
